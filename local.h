02a32a5409a88244025cdc61ef2033534a978f2e
rob <rob@dijital.ca>
2017-10-27 09:46:38 -0700

    First commit.

---
+++ local.h
@@ -0,0 +1,5 @@
+void filldir(char*, char*, int, struct band3 *);
+void resolve(char*, int, struct band3 *);
+int dopolys(char*, char*, int, int);
+void wtrshed(char*, char*, int, int, int);
+void ppupdate(char*, char*, int, int, struct band3 *, struct band3 *);
